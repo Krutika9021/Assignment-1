5c1d2a1a7d55d4f5390c8e58ffd98a5c3e5ed19c
Krutika9021 <109949451+Krutika9021@users.noreply.github.com>
2022-08-04 23:45:25 -0800

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,16 @@
+//Write a program in JAVA to print a welcome text in a separate line.
+
+
+public class sum 
+{
+    public static void main(String args[])
+    {
+        int a = 29,b = 30,sum;
+        System.out.println("Enter first number : " + a);
+        System.out.println("Enter second number : " + b);
+        
+        sum = a+b;
+        System.out.println("Sum of" + a + " and" + b + " is : " + sum);
+               
+    }
+}
