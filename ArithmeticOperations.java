8811c11ca68391fc73221c83efa696885c4d6838
Krutika9021 <109949451+Krutika9021@users.noreply.github.com>
2022-08-08 07:43:28 -0800

    Add files via upload

---
+++ ArithmeticOperations.java
@@ -0,0 +1,21 @@
+//Write a program in JAVA to display various type or arithmetic operation using
+
+
+public class ArithmeticOperations
+{
+      public static void main(String args[])
+    {
+        int a=10;
+        int b=20;
+        int c;
+        
+        c=a+b;
+        System.out.println("addtion : "+c);
+        c=a-b;
+        System.out.println("Substraction : "+c);
+        c=a/b;
+        System.out.println("Division : "+c);
+        c=a*b;
+        System.out.println("Multiplication : "+c);
+    }
+}
---
+++ SumUsingVariables.java
@@ -0,0 +1,12 @@
+
+//Write a program in JAVA to print the sum of two numbers using variables
+
+public class SumUsingVariables 
+{
+    public static void main(String args[])   
+{  
+int n1 = 225, n2 = 115, sum;  
+sum = n1 + n2;  
+System.out.println("The sum of numbers is: "+sum);  
+}  
+}
